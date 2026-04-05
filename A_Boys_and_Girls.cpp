4f2438491b66a098390c47783a970401899a0b15
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2026-04-05 22:17:22 +0530

    5 apr part 1

---
+++ A_Boys_and_Girls.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (auto& i : v) is >> i;
+    return is;
+}
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (int i = 0; i < (int)v.size(); i++) os << (i ? " " : "") << v[i];
+    return os;
+}
+
+#ifdef LOCAL
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A,B>& p){
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    os << '[';
+    for (int i = 0; i < (int)v.size(); i++){
+        if (i) os << ", ";
+        os << v[i];
+    }
+    return os << ']';
+}
+inline void dbg_out(){ cerr << '\n'; }
+template<typename Head, typename... Tail>
+inline void dbg_out(Head H, Tail... T){
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void)0)
+#endif
+
+void solve() {
+    // THINK FIRST, CODE LATER
+    // 1) constraints?
+    // 2) edge cases?
+    // 3) approach in 2 lines
+    int b,g;cin >>b>>g;
+    
+    if(b==g){
+        for(int i=0;i<b;i++){
+            cout <<('B');
+            cout <<('G');
+        }
+    }
+    else if(b>g){
+         for(int i=0;i<g;i++){
+            cout <<('B');
+            cout <<('G');
+        }
+        b= b-g;
+        while(b--){
+            cout <<('B');
+        }
+    }
+    else{
+         for(int i=0;i<b;i++){
+            cout <<('G');
+            cout <<('B');
+        }
+        g= g-b;
+        while(g--){
+            cout <<('G');
+        }
+    }
+
+    cout <<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
