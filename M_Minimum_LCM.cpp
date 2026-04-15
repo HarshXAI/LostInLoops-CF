20943e62bbfac5fd8b5c9b660924d3b07982d0da
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2026-04-15 23:42:16 +0530

    15 apr ka

---
+++ M_Minimum_LCM.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (auto& i : v) is >> i;
+    return is;
+}
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (int i = 0; i < (int)v.size(); i++) os << (i ? " " : "") << v[i];
+    return os;
+}
+
+#ifdef LOCAL
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A,B>& p){
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    os << '[';
+    for (int i = 0; i < (int)v.size(); i++){
+        if (i) os << ", ";
+        os << v[i];
+    }
+    return os << ']';
+}
+inline void dbg_out(){ cerr << '\n'; }
+template<typename Head, typename... Tail>
+inline void dbg_out(Head H, Tail... T){
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void)0)
+#endif
+void solve() {
+    long long n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << n/2 << " " << n/2 << "\n";
+        return;
+    }
+
+    // find smallest divisor
+    for (long long i = 3; i * i <= n; i++) {
+        if (n % i == 0) {
+            long long a = n / i;
+            cout << a << " " << n - a << "\n";
+            return;
+        }
+    }
+
+    // prime case
+    cout << 1 << " " << n - 1 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
