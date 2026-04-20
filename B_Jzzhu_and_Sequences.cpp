7fcd2f883165f5ecd1cfc48aa620d401ba1635d8
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2026-04-20 15:14:54 +0530

    20 apr ka part 1

---
+++ B_Jzzhu_and_Sequences.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (auto& i : v) is >> i;
+    return is;
+}
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (int i = 0; i < (int)v.size(); i++) os << (i ? " " : "") << v[i];
+    return os;
+}
+
+#ifdef LOCAL
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A,B>& p){
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    os << '[';
+    for (int i = 0; i < (int)v.size(); i++){
+        if (i) os << ", ";
+        os << v[i];
+    }
+    return os << ']';
+}
+inline void dbg_out(){ cerr << '\n'; }
+template<typename Head, typename... Tail>
+inline void dbg_out(Head H, Tail... T){
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void)0)
+#endif
+
+void solve() {
+    ll x, y;
+    cin >> x >> y;
+    ll n;
+    cin >> n;
+
+    const ll MOD = 1e9 + 7;
+
+    ll f[6];
+    f[0] = x;
+    f[1] = y;
+    f[2] = y - x;
+    f[3] = -x;
+    f[4] = -y;
+    f[5] = x - y;
+
+    ll ans = f[(n - 1) % 6];
+
+    ans = (ans % MOD + MOD) % MOD;
+
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
